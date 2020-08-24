528e844c2ba1fd9b923921de47822510c865136a
Azhar Bhatti <azharbhatti2016@gmail.com>
2020-08-24 19:00:28 +0500

    Updates

---
+++ precache-manifest.421fb245a59b3a7cd986ccc9a530160a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c32c73eeda3eb0f2630a6870f3063bd7",
+    "url": "/groceryDone4u/index.html"
+  },
+  {
+    "revision": "3492f5f62c63c227d46b",
+    "url": "/groceryDone4u/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "933b12dc737047e435e1",
+    "url": "/groceryDone4u/static/css/main.646da39e.chunk.css"
+  },
+  {
+    "revision": "3492f5f62c63c227d46b",
+    "url": "/groceryDone4u/static/js/2.b0888340.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/groceryDone4u/static/js/2.b0888340.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "933b12dc737047e435e1",
+    "url": "/groceryDone4u/static/js/main.b197fc48.chunk.js"
+  },
+  {
+    "revision": "0a77ef63abcf009fbc16",
+    "url": "/groceryDone4u/static/js/runtime-main.7690d402.js"
+  },
+  {
+    "revision": "5a308170262af5552e44fcd45255238a",
+    "url": "/groceryDone4u/static/media/banner-campaign-3.5a308170.png"
+  },
+  {
+    "revision": "7cec72e4bd7ec90ef4ffff52eaa2e50d",
+    "url": "/groceryDone4u/static/media/banner-campaign-4.7cec72e4.png"
+  },
+  {
+    "revision": "e91b14914a1b7857be97c2a4aaae8183",
+    "url": "/groceryDone4u/static/media/banner_bg_3.e91b1491.png"
+  },
+  {
+    "revision": "3ecc484b11232b1ab7cc41777dcb7117",
+    "url": "/groceryDone4u/static/media/doodle_background.3ecc484b.jpg"
+  },
+  {
+    "revision": "67c1e4863e4c1eb2427cda627f2d1431",
+    "url": "/groceryDone4u/static/media/head_phone_1.67c1e486.png"
+  },
+  {
+    "revision": "2ce895c9c2702a9d1944a6de0a1da28b",
+    "url": "/groceryDone4u/static/media/promo_overlay.2ce895c9.png"
+  }
+]);
